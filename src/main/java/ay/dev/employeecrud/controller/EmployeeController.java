fa3f3b19866e7d90d82c2af47dfe01febfab7719
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-04-06 02:19:09 +0200

    add and update employee

--- src/main/java/ay/dev/employeecrud/controller/EmployeeController.java
+++ src/main/java/ay/dev/employeecrud/controller/EmployeeController.java
@@ -5,6 +5,9 @@ import ay.dev.employeecrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -15,12 +18,37 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @GetMapping({"/showEmployees", "/", ""})
+    @GetMapping({"/showEmployees", "/", "/list"})
     public ModelAndView showEmployees() {
-        ModelAndView mav = new ModelAndView("list-employees");
+        ModelAndView mAv = new ModelAndView("list-employees");
         List<Employee> list = employeeRepository.findAll();
-        mav.addObject("employees", list);
+        mAv.addObject("employees", list);
 
-        return mav;
+        return mAv;
+    }
+
+    @GetMapping("/addEmployeeForm")
+    public ModelAndView addEmployeeForm() {
+        ModelAndView mAv = new ModelAndView("add-employee-form");
+        Employee employee = new Employee();
+        mAv.addObject("employee", employee);
+        mAv.getModel().put("pageTitle", "Add Employee" );
+
+        return mAv;
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute Employee employee) {
+        employeeRepository.save(employee);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/showUpdateForm")
+    public ModelAndView updateEmployeeForm(@RequestParam Long employeeId) {
+        ModelAndView mAw = new ModelAndView("add-employee-form");
+        Employee employee = employeeRepository.findById(employeeId).get();
+        mAw.addObject("employee", employee);
+        mAw.getModel().put("pageTitle", "Update Employee (ID: " + employeeId + ")" );
+        return mAw;
     }
 }
